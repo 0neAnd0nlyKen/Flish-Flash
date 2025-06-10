7f37db797dc2d6c38660a30cbda3a525243f19bf
0neAnd0nlyKen <kendrickraphael@gmail.com>
2025-06-10 14:21:51 +0800

    define interface for game details

---
+++ frontend/types/GameDetails.tsx
@@ -0,0 +1,13 @@
+export interface GameDetails {
+    file: any;
+    // swf?: Blob | null; // Optional SWF blob, can be null if not decoded
+  //   genres: string[];
+  // id: number;
+  // title: string;
+
+//   description: string;
+//   releaseDate: string;
+//   developer: string;
+//   publisher: string;
+//   screenshots: string[];
+}
