20f8dcfec90bfdf002ecd9fcbe8026f4b0710746
0neAnd0nlyKen <kendrickraphael@gmail.com>
2025-06-10 14:23:49 +0800

    frontend game feed uses server component to fetch game

--- frontend/components/GamesFeed.tsx
+++ frontend/components/GamesFeed.tsx
@@ -7,6 +7,9 @@ import {
 } from "@/components/ui/carousel"
 
 import GamePlayer from './GamePlayer.js';
+import { getGames } from "../actions/GetGames";
+import { GameDetails } from "../types/GameDetails";
+import { log } from "console";
 // import GameCarouselItems from "./GameCarouselItems"
 // import { getGames } from "../actions/GetGames"
 // import { GamePreferences } from "../types/GamePreferences"
@@ -18,7 +21,14 @@ import GamePlayer from './GamePlayer.js';
 //     playTime: [],
 // }
 
+//use GetGames
+const initialGames = await getGames().then((data) => {
+    console.log("yo INITIAL GAMES", data);
+    return data; // Return the first game for now
+});
+
 export default function GameFeed() {
+    console.log("GAME FEED")
 //   const initialGames = await getGames(preferences);
 
   return (
@@ -26,9 +36,7 @@ export default function GameFeed() {
       <Carousel>
         <CarouselContent>
             <CarouselItem key={1}>
-                <GamePlayer swfPath={"http://localhost:8080/games/1"}>
-
-                </GamePlayer>
+                <GamePlayer swfPath={initialGames[0].file} />
             </CarouselItem>
 
         </CarouselContent>
