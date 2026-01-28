ee76113923d46722bb26b2fd4cf4e0f305c9455f
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-01-28 22:21:51 +0800

    temp game feed just for billiard

--- frontend/components/GamesFeed.tsx
+++ frontend/components/GamesFeed.tsx
@@ -23,10 +23,15 @@ import GamePlayerContainer from "./GamePlayerContainer";
 // }
 
 //use GetGames
-const initialGames = await getGames().then((data) => {
-    console.log("yo INITIAL GAMES", data);
-    return data; // Return the first game for now
-});
+// const initialGames = await getGames().then((data) => {
+//     console.log("yo INITIAL GAMES", data);
+//     return data; // Return the first game for now
+// });
+
+// Example GameDetails object with fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/games/2_billiards-2-play/swf`
+const billiards_game: GameDetails = {
+    file: `${process.env.NEXT_PUBLIC_API_URL}/v1/games/2_billiards-2-play/swf`
+};
 
 export default function GameFeed() {
     console.log("GAME FEED")
@@ -36,12 +41,26 @@ return (
     <div className="game-feed">
       <Carousel>
         <CarouselContent>
-            {initialGames.map((game: GameDetails, index: number) => (
+            {/* {initialGames.map((game: GameDetails, index: number) => (
               <CarouselItem key={index}>
                 <GamePlayerContainer game={game} />
               </CarouselItem>
-            ))}
+            ))} */}
+            {/* 10 instance of CarouselItem each containing GamePlayerContainer game={fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/games/2_billiards-2-play/swf`)}*/}
+            <CarouselItem key ={1}>
+                <GamePlayerContainer game={billiards_game} />
+            </CarouselItem>
+            <CarouselItem key ={2}>
+                <GamePlayerContainer game={billiards_game} />
+            </CarouselItem>
+            <CarouselItem key ={3}>
+                <GamePlayerContainer game={billiards_game} />
+            </CarouselItem>
+            <CarouselItem key ={4}>
+                <GamePlayerContainer game={billiards_game} />
+            </CarouselItem>
         </CarouselContent>
       </Carousel>
     </div>
   )
+}
