865e41c736a2792afe816130f21b7d8d2795aadc
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-01-30 02:33:50 +0800

    container layout with metadata

--- frontend/components/GamePlayerContainer.tsx
+++ frontend/components/GamePlayerContainer.tsx
@@ -2,9 +2,23 @@
 
 import { GameDetails } from "../types/GameDetails";
 import GamePlayer from "./GamePlayer";
+import styles from "./GamePlayerContainer.module.css";
 
 export default function GamePlayerContainer({ game }: { game: GameDetails }) {
     return(
-        <GamePlayer swfPath={game.file} />
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <h1>{game.title}</h1>
+                <img src={game.previewImage} alt={game.title} className={styles.previewImage} />
+            </div>
+            <div className={styles.middle}>
+                <div className={styles.ruffle}>
+                    <GamePlayer swfPath={game.file} />
+                </div>
+            </div>
+            <div className={styles.right}>
+                <div dangerouslySetInnerHTML={{ __html: game.description }} />
+            </div>
+        </div>
     )
 }
