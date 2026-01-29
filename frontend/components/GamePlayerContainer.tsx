96525b6793ba2befc3ea210c999e2b91d6825e63
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-01-30 04:42:09 +0800

    update style w/ pico

--- frontend/components/GamePlayerContainer.tsx
+++ frontend/components/GamePlayerContainer.tsx
@@ -1,4 +1,5 @@
 'use server'
+import "@picocss/pico/css/pico.min.css";
 
 import { GameDetails } from "../types/GameDetails";
 import GamePlayer from "./GamePlayer";
@@ -6,17 +7,19 @@ import styles from "./GamePlayerContainer.module.css";
 
 export default function GamePlayerContainer({ game }: { game: GameDetails }) {
     return(
-        <div className={styles.container}>
+        <div className={`${styles.container} container`}>
             <div className={styles.left}>
-                <h1>{game.title}</h1>
-                <img src={game.previewImage} alt={game.title} className={styles.previewImage} />
+                <h2>{game.title}</h2>
+                <figure>
+                  <img src={game.previewImage} alt={game.title} className={styles.previewImage} />
+                </figure>
             </div>
             <div className={styles.middle}>
                 <div className={styles.ruffle}>
                     <GamePlayer swfPath={game.file} />
                 </div>
             </div>
-            <div className={styles.right}>
+            <div className={`${styles.right} flow`}>
                 <div dangerouslySetInnerHTML={{ __html: game.description }} />
             </div>
         </div>
